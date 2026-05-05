08a51ad91d09e7b2463a3fd530b23923f3ea4624
Anthony <mirandanthony2005@yahoo.fr>
2026-05-05 15:15:06 +0200

    doc anglais

--- detecVisage.py
+++ detecVisage.py
@@ -3,23 +3,47 @@ from mtcnn import MTCNN
 from  mtcnn.utils.images  import  load_image
 
 
-def deteceterVisage(url_img ):
+def FacesDetects(url_img : str ):
+    """
+    Detects faces in an image using the MTCNN algorithm.
+
+    Args:
+        url_img (str): The local path to the image file.
+
+    Returns:
+        tuple: 
+            - result (list): A list of dictionaries returned by MTCNN. Each dictionary contains 
+              'box', 'confidence', and 'keypoints' for each detected face.
+            - image (numpy.ndarray): The loaded image data in RGB format.
+    """
     detector  =  MTCNN ( device = "CPU:0" )
     image  =  load_image ( url_img )
     result  =  detector.detect_faces ( image )
     return result , image
 
-def dessinVisage(img, liste_visage):
-    img_copy = img.copy()
-    for visage in liste_visage:
-        x, y, w, h = visage['box']
+def FacesDraw(image, list_faces):
+    """
+    Draws bounding boxes around detected faces on the image.
+    
+    Args:
+        - image (numpy.ndarray): The original image data in RGB format.
+        - result (list): A list of dictionaries returned by MTCNN. Each dictionary contains 
+        'box', 'confidence', and 'keypoints' for each detected face.
+
+    Returns:
+        - img_copy (numpy.ndarray): A copy of the input image with bounding boxes drawn.
+
+    """
+    img_copy = image.copy()
+    for face in list_faces:
+        x, y, w, h = face['box']
         cv2.rectangle(img_copy, (x, y), (x + w, y + h), (0, 255, 0), 2)
     return img_copy
 
 
 url = "images/foule.jpg"
-result , image =deteceterVisage(url)
-image_cadree = dessinVisage(image , result)
+result , image =FacesDetects(url)
+image_cadree =  FacesDraw(image , result)
 
 image_bgr = cv2.cvtColor(image_cadree, cv2.COLOR_RGB2BGR)
 
