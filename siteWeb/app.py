de01aa0f1e5f89016d4c802201d154ad41451e17
Anthony <mirandanthony2005@yahoo.fr>
2026-05-18 14:16:56 +0200

    load model

---
+++ siteWeb/app.py
@@ -0,0 +1,110 @@
+import sys
+import io
+import cv2
+sys.path.append('../')  # add parent directory to path to import detecVisage
+from fastapi import FastAPI, UploadFile, File, Form, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import StreamingResponse
+from detecVisage import FacesDetects_from_bytes
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+import mediapipe as mp
+from faceAlignment import align_crop
+from embeddings import get_embedding
+from qdrant_db import save_embedding, create_collection, search_embedding
+from DrawBox import  DrawBox , color_name_to_rgb
+from bodyDetection import BodyDetect_from_bytes
+from bodyAlignment import body_crop
+from tracker import BodyTracker
+
+
+
+
+# create the FastAPI application
+app = FastAPI()
+
+model_path_blazeface='../model/blaze_face_short_range.tflite'
+model_path_yolov = cv2.dnn.readNetFromONNX("../model/yolov8n.onnx")
+
+
+base_options = python.BaseOptions(model_asset_path=model_path_blazeface)
+options = vision.FaceDetectorOptions(base_options=base_options)
+detector = vision.FaceDetector.create_from_options(options)
+
+# CORS — allows the browser to send requests to FastAPI
+# without this, the browser blocks requests for security reasons
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# ── ROUTE /add ──
+# receives : firstName (text) + lastName (text) + photo (file)
+# returns  : a confirmation message
+@app.post("/add")
+async def add_person(
+    firstName: str = Form(...),
+    lastName:  str = Form(...),
+    photo:     UploadFile = File(...)
+):
+    contents = await photo.read()
+    boxes_face, result, image = FacesDetects_from_bytes(contents,"mediapipe",detector)
+
+    image_boxed = DrawBox(image, boxes_face, 'green')
+
+    # convert the boxed image to bytes
+    image_boxes_bgr = cv2.cvtColor(image_boxed, cv2.COLOR_RGB2BGR)
+    _, buffer = cv2.imencode('.jpg', image_boxes_bgr)
+    image_bytes = buffer.tobytes()
+
+    #print(f"Received: {firstName} {lastName}, file: {photo.filename}")
+    crops = align_crop(image, result)
+    create_collection()
+
+    for face_cropped in crops:
+        embedding = get_embedding(face_cropped)
+        #print(f"Embedding shape: {embedding.shape}")
+    save_embedding(f"{firstName} {lastName}".strip().lower(), embedding)
+
+    # sends image to browser
+    return StreamingResponse(io.BytesIO(image_bytes), media_type="image/jpeg")
+
+@app.websocket("/ws/detect")
+async def detec_video(websocket: WebSocket):
+    await websocket.accept()
+    tracker = BodyTracker()
+    while True:
+        data = await websocket.receive_bytes()
+        boxes_face ,result, image = FacesDetects_from_bytes(data,"mediapipe",detector)
+        boxes_body, confidence, image = BodyDetect_from_bytes(data, model_path_yolov)
+
+        names = [""] * len(boxes_face)
+        clean_names = [""] * len(boxes_face)
+        if result and result.detections:
+            crops = align_crop(image, result)
+            for i, face_cropped in enumerate(crops):
+                embedding = get_embedding(face_cropped)
+                name, score = search_embedding(embedding)
+                score_str = f"{score:.2f}" if score else "?"
+                names[i] = f"{name} ({score_str})"
+                clean_names[i] = name
+        crops_body = body_crop(image, boxes_body) if boxes_body else []
+        body_names = tracker.update(boxes_face, boxes_body, clean_names, crops_body)
+        #print(f"body_names: {body_names}")
+        #print(f"clean_names: {clean_names}")
+        await websocket.send_json({
+            "faces": boxes_face , 
+            "body":boxes_body ,  
+            "names": names,
+            "body_names": body_names
+        })
+
+
+
+
+
+app.mount("/static", StaticFiles(directory=".", html=True), name="static")
+
