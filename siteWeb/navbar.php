53cf7375cf3b45c0e9d7e945e7b31341443049b1
Anthony <mirandanthony2005@yahoo.fr>
2026-06-10 10:58:36 +0200

    debut php

---
+++ siteWeb/navbar.php
@@ -0,0 +1,10 @@
+<nav class="navbar">
+    <div class="navbar-inner">
+        <span class="nav-brand">Face Recognition</span>
+        <div class="nav-links">
+            <a href="home.html" class="nav-link">Home</a>
+            <a href="index.html" class="nav-link active">Register</a>
+            <a href="detection.html" class="nav-link">Detect</a>
+        </div>
+    </div>
+</nav>
